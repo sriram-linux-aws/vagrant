8c04a1be230cfad4699393de67b25629914363ee
sriram-linux-aws <sriram.linux.aws@gmail.com>
2016-07-21 16:44:08 -0600

    Update ansible-multivms.sh

--- scripts/ansible-multivms.sh
+++ scripts/ansible-multivms.sh
@@ -14,3 +14,8 @@ SSHEOF
 
 chown -R vagrant:vagrant /home/vagrant/.ssh/
 
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
+
